98bb04a08c04d59e51e2797ad9e78c0083202d68
Aleksandr <Aleksandr.zaichikov@simbirsoft.com>
2023-05-16 21:17:39 +0400

    Class SelenideTest done

--- src/test/java/com/zaychikov/tests/SelenideTest.java
+++ src/test/java/com/zaychikov/tests/SelenideTest.java
@@ -1,4 +1,39 @@
 package com.zaychikov.tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
 public class SelenideTest {
+
+    private final String Url = "https://github.com";
+    private final String Directory = "Prog163/tests_with_allure";
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @AfterAll
+    static void afterAll() {
+        //Оставляю браузер открытым для проверки введённых значений
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    public void issueSearchTestInGithub() {
+        open(Url);
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(Directory);
+        $(".header-search-input").submit();
+        $(linkText(Directory)).click();
+        $("#issues-tab").shouldHave(text("Issues"));
+    }
 }
